413a111c2928cefaa0b4acdf0f1410b48885013d
meteaure studios <48535478+meteaurestudios@users.noreply.github.com>
2019-10-23 16:27:06 +0200

    display registered message to confirm form submit

---
+++ script.js
@@ -0,0 +1,64 @@
+function openAboutPane() {
+	var x = document.getElementById("about_pane");
+	x.style.display = "block";
+}
+
+function closeAboutPane() {
+	var x = document.getElementById("about_pane");
+	x.style.display = "none";
+	window.location.hash = ""
+}
+
+function openJoinBetaPane() {
+	var x = document.getElementById("joinbeta_pane");
+	x.style.display = "block";
+	window.location.hash = "join";
+}
+
+function closeJoinBetaPane() {
+	var x = document.getElementById("joinbeta_pane");
+	x.style.display = "none";
+	window.location.hash = ""
+}
+
+function validateEmail() {
+	var email = document.getElementById("email").value;
+	var valid = /^\S+@gmail\.com$/.test(email);
+
+	if(!valid) {
+		var x = document.getElementById("email-error");
+		x.style.display = "block";
+	}
+	else {
+		var redirect = document.getElementById("redirect_form");
+		var url = window.location.href.split("#")[0];
+		redirect.value = url + "#registered"
+		
+	}
+
+	return valid;
+}
+
+function displayRegistered() {
+	var join_button = document.getElementById("beta-button");
+	join_button.className = "green_button";
+	join_button.innerHTML = "REGISTERED"
+
+	var joined_text = document.getElementById("email-registered");
+	joined_text.style.display = "block";
+}
+
+window.onload = function() {
+
+    var element = window.location.hash;
+
+    if(element == "#registered") {
+    	displayRegistered();
+    }
+    else if(element == "#join") {
+    	openJoinBetaPane();
+    }
+    else if(element == "#about") {
+    	openAboutPane();
+    }
+}
